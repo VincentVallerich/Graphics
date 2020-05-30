529cc0d32cc904b2daeb0eaf30b931af625e7bd1
FoxHawnd <nisoriku@gmail.com>
2020-05-30 18:41:13 +0200

    addition of a thickness textfield and correction of the cursor bug

--- graphics/extensions/Paint.java
+++ graphics/extensions/Paint.java
@@ -3,6 +3,9 @@ package graphics.extensions;
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import graphics.shapes.ui.ShapeDraftman;
 import java.awt.event.MouseMotionListener;
 import graphics.shapes.SCircle;
@@ -16,50 +19,56 @@ import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.awt.GridLayout;
 
-
-public class Paint extends JFrame implements MouseMotionListener {
-	private int x = 0, y=0;
+public class Paint extends JFrame implements MouseMotionListener,ActionListener {
+	private int x = 0, y = 0;
 	public Color co;
-	public JButton Colors;
-	public JColorChooser JColors;
-	
+	public JButton colors;
+	public JColorChooser jColors;
+	public JTextField thickness;
+	public String t = "1";
 	public Paint() {
-		
-		setSize(1200,720);
+
+		setSize(1200, 720);
 		setResizable(false);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		JPanel jp = new JPanel();
+		jp.setLayout(new GridLayout(1,2));
+		colors = new JButton("Select a color");
+		thickness = new JTextField(t,10);
+		colors.addActionListener(this);
+		thickness.addActionListener(this);
+		jp.add(colors);
+		jp.add(thickness);
 		Container c = this.getContentPane();
 		c.setLayout(new BorderLayout());
 		c.addMouseMotionListener(this);
-		Colors = new JButton("Select a color");
-		Colors.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				co = JColorChooser.showDialog(null,"Select a color",Color.BLACK);
-			}
-		});
-		add(Colors,BorderLayout.NORTH);
+		c.add(jp,BorderLayout.NORTH);
 		setVisible(true);
 		
 	}
 
 	@Override
-	public void mouseDragged(MouseEvent e) {
+	public void mouseDragged(final MouseEvent e) {
 		x=e.getX();
 		y=e.getY();
 		repaint();
 	}
 
 	@Override
-	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void mouseMoved(final MouseEvent e) {
 	}
 	
-	public void paint(Graphics g) {
-		Point p = new Point(x,y);
-		ShapeDraftman sd = new ShapeDraftman(g);
-		SCircle sc = new SCircle(p,4);
+	public void actionPerformed(final ActionEvent e) {
+		co = JColorChooser.showDialog(null,"Select a color",Color.BLACK);
+		t = thickness.getText();
+	}
+
+	public void paint(final Graphics g) {
+		final Point p = new Point(x+15,y+45);
+		final ShapeDraftman sd = new ShapeDraftman(g);
+		final SCircle sc = new SCircle(p,(int)Integer.parseInt(t));
 		sc.addAttributes(new ColorAttributes(true,true,co,co));
 		sc.addAttributes(new SelectionAttributes());
 		sd.visitCircle(sc);
